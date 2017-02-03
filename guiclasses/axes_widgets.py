8b4d5e61e3cb69950744ef8f8ce6c40cdef14a91
aafanase <Arseniy.Afanasenko@nominum.com>
2017-02-03 15:26:43 +0300

    Add method to plot signal with markup

--- guiclasses/axes_widgets.py
+++ guiclasses/axes_widgets.py
@@ -43,3 +43,12 @@ class axesWidget(FigureCanvas):
         self.axes.plot(t, x)
         self.draw()
         pass
+
+    def plot_signal_with_markup(self, x, marks, fs):
+        t = np.array(range(0, x.shape[0])) / fs
+        self.axes.plot(t, x, "b")
+        self.axes.hold(True)
+        self.axes.plot(marks/fs, [x[i] for i in marks], "r+", markersize=6)
+        self.axes.hold(False)
+        self.draw()
+        pass
