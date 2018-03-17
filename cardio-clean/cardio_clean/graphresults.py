7b89d9b8389f8967ca640e83e9b3e7553256fee8
aafanase <Arseniy.Afanasenko@nominum.com>
2018-03-17 12:05:16 +0300

    Add qrs plotting

--- cardio-clean/cardio_clean/graphresults.py
+++ cardio-clean/cardio_clean/graphresults.py
@@ -7,11 +7,10 @@ import numpy as np
 
 from matplotlib import pyplot as plt
 from sigbind import build_comb_filter, mean_spectrum, mains_filter
+from qrsdetect import *
 
 
-def main():
-
-    recordname = sys.argv[1]
+def show_spectrums(recordname):
 
     sig, fields = wfdb.rdsamp(recordname)
     sp = mean_spectrum(sig[:, 0])
@@ -42,6 +41,55 @@ def main():
     axarr[0,1].plot(x[:happ], y[:happ], 'r')
 
     plt.show()
+
+
+def show_qrs(recordname):
+
+    # загрузили сигнал
+    sig, fields = wfdb.rdsamp(recordname)
+    fs = fields["fs"]
+
+    # следующие две строчки для наглядности. В реальной прорамме достаточно
+    # вызвать
+    # meta, foo = qrs_detection(sig, fs)
+
+    ptstat = qrs_preprocessing(sig, fs)
+    meta, strobe = qrs_detection(sig, fs)
+
+    # номера начального и конечного отсчета для отображения
+    N1 = 0
+    N2 = 900
+
+    # номер канала
+    chan = 0
+
+    tt = np.linspace(float(N1)/fs, float(N2-1)/fs, N2-N1)
+
+    plt.rcParams["figure.facecolor"] = "white"
+    fig, axarr = plt.subplots(2, 1)
+
+    axarr[0].plot(tt, sig[N1:N2, chan])
+
+    # пометим R-зубцы
+    rx = []
+    ry = []
+    for x in meta:
+        if x["R-peak"] <= tt[-1]:
+            rx.append(x["R-peak"])
+            ry.append(sig[int(rx[-1]*fs), chan])
+
+    axarr[0].scatter(rx, ry)
+
+    # решающая статистика и стробы QRS-комплексов
+    axarr[1].plot(tt,ptstat[N1:N2])
+    axarr[1].plot(tt,strobe[N1:N2])
+
+    plt.show()
+
+
+def main():
+    #show_spectrums(sys.argv[1])
+    show_qrs("/Users/arseniy/Downloads/ROXMINE/Rh2010")
 
 
 if __name__ == "__main__":
