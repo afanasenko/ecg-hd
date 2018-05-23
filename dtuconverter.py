5f19542390331c15644a1dc42d4ad3d11f31c94a
lenaumova <38700578+lenaumova@users.noreply.github.com>
2018-05-23 13:50:34 +0300

    Update dtuconverter.py

--- dtuconverter.py
+++ dtuconverter.py
@@ -15,13 +15,13 @@ if __name__ == "__main__":
         
         d, fs1, n = dturead(fn)
         signal = d
-
+        
         n_sig = 6
         wfdb.wrsamp("name", fs = 4000, units=["mV"]*n_sig,
                     sig_name=["ch1otv1", "ch1otv2", "ch1otv3", "ch2ovt1", "ch2otv2", "ch2otv3"],
                     p_signal = signal, fmt=["16"]*n_sig, write_dir="directory")
         signals, fields = wfdb.rdsamp("directory\name", sampfrom=0, sampto="end", channels="all")
-
+        
         fs = 4000
         t = np.arange(0, len(signals)/fs, 1.0/fs)
         plt.plot(t, signals)
@@ -29,7 +29,6 @@ if __name__ == "__main__":
         plt.title("Signals")
         print("Look at the plots")
         plt.show()
-
+        
     else:
         print("Задайте файл")
-
