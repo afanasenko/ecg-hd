fbbf8bcb7a86611079ce6284f0512e1792884b53
lenaumova <38700578+lenaumova@users.noreply.github.com>
2018-05-23 13:53:33 +0300

    Update zetlabconverter.py

--- zetlabconverter.py
+++ zetlabconverter.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
             d, fs, n = anaread(fn)
             #print("d=")
             #print(d)
-            sig = np.concatenate((sig, d)) 
+            sig = np.concatenate((sig, d))
             #print("sig=")
             #print(sig)
         
@@ -52,4 +52,3 @@ if __name__ == "__main__":
         
     else:
         print("Задайте несколько файлов")
-
