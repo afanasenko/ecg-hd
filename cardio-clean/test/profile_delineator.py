d24776c023b7649300d05ae0c6cae37c1032c3fa
afanasenko <afanasenko@smedx.com>
2018-06-15 13:14:05 +0300

    Добавлен скрипт для профилирования

---
+++ cardio-clean/test/profile_delineator.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+import cProfile as profile
+
+from cardio_clean.wavdetect import find_points
+from cardio_clean.qrsdetect import qrs_detection
+from cardio_clean.graphresults import ecgread
+
+pr = profile.Profile()
+
+sig, header = ecgread("/Users/arseniy/SERDECH/data/ROXMINE/Rh1011")
+fs = header["fs"]
+if fs != 250:
+    print("Warning! fs={}".format(fs))
+
+s = sig[:, 0]
+
+metadata, debugdata = qrs_detection(
+    sig[:, :],
+    fs=header["fs"],
+    bias=header["baseline"],
+    gain=header["adc_gain"],
+    minqrs_ms=20)
+
+pr.enable()
+newmeta = find_points(s, header["fs"], metadata)
+pr.disable()
+
+pr.dump_stats('profile.pstat')
