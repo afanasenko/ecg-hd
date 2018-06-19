48faaf4280256592fee40892d0a5accf4837a928
afanasenko <afanasenko@smedx.com>
2018-06-19 09:51:00 +0300

    Перенос демо-скриптов

--- cardio-clean/test/demo_analysis.py
+++ cardio-clean/test/demo_analysis.py
@@ -1,201 +1,196 @@
-#!/usr/bin/anv python
 # coding: utf-8
 
-import wfdb
-import sys
 import numpy as np
-
-from argparse import ArgumentParser
 from matplotlib import pyplot as plt
-from cardio_clean.sigbind import build_comb_filter, mean_spectrum, \
-    mains_filter, \
-    fix_baseline
-from cardio_clean.qrsdetect import *
-from cardio_clean.cardioproc_api import read_buffer
+from scipy.signal import lfilter, hann
+from scipy.fftpack import fft
+
+from graphresults import ecgread
+from wavdetect import ddwt, find_points, zcfind
+from qrsdetect import qrs_detection
 
 
-def ecgread(filename):
-    if filename.endswith(".ecg"):
-        with open(filename, "rb") as fi:
-            hdr, data = read_buffer(fi)
-            return data, hdr
+def dummy_shift(x, n):
+    return np.concatenate((x[n:], np.ones(n)*x[-1]))
 
+
+def ddgau(sigma, fs, ksigma=3):
+
+    dt = 1.0 / fs
+    halfw = sigma*ksigma
+
+    x = np.arange(-halfw, halfw, dt)
+    y = np.zeros(len(x))
+    sumsq = 0.0
+
+    for i, t in enumerate(x):
+        tt = t*t/(2.0*sigma*sigma)
+        y[i] = (1-(t/sigma)**2)*np.exp(-tt)
+        sumsq += y[i]*y[i]
+
+    return x, y/sumsq
+
+
+def dgau(sigma, fs, ksigma=3):
+
+    dt = 1.0 / fs
+    halfw = sigma*ksigma
+
+    x = np.arange(-halfw, halfw, dt)
+    y = np.zeros(len(x))
+    sumsq = 0.0
+
+    for i, t in enumerate(x):
+        tt = t/sigma
+        y[i] = -t*np.exp(-tt*tt)
+        sumsq += y[i]*y[i]
+
+    return x, y/sumsq
+
+
+def gaussogram(x):
+
+    sigmas = np.array([
+        0.009,
+        0.015,
+        0.03,
+        0.06
+    ])
+
+    fig, axarr = plt.subplots(len(sigmas)+1, 1, sharex=True)
+
+    filterbank = []
+    for i, s in enumerate(sigmas):
+        t, h = dgau(s,250)
+        filterbank.append(h)
+
+    axarr[0].plot(x)
+
+    for i, h in enumerate(filterbank):
+
+        sm = lfilter(h, [1.0], x)
+        sm = dummy_shift(sm, int(len(h)/2))
+
+        axarr[i+1].plot(sm)
+        axarr[i+1].grid()
+    plt.show()
+
+
+def multiplot(siglist):
+    fig, axarr = plt.subplots(len(siglist), 1, sharex=True)
+    for i, s in enumerate(siglist):
+        axarr[i].plot(s)
+        axarr[i].grid()
+    print("Look at the plots")
+    plt.show()
+
+
+def multispectrum(siglist):
+    fig, axarr = plt.subplots(len(siglist), 1, sharex=True)
+    for i, s in enumerate(siglist):
+        n1 = len(s)
+        n2 = int(n1/2)
+        wgt = np.array(hann(n1))
+        yf = fft(s * wgt)
+        yf = np.abs(yf[0:n2])
+        axarr[i].plot(yf)
+        axarr[i].grid()
+    print("Look at the plots")
+    plt.show()
+
+
+def multilinespec(siglist):
+    leg = []
+    for i, s in enumerate(siglist):
+        n1 = len(s)
+        n2 = int(n1/2)
+        wgt = np.array(hann(n1))
+        yf = fft(s * wgt)
+        yf = np.abs(yf[0:n2])
+        plt.plot(yf)
+        leg.append(str(i))
+    plt.grid()
+    plt.legend(leg)
+    print("Look at the plots")
+    plt.show()
+
+
+def show_filter_responses(spectral=False):
+
+    T = 256
+    x = np.zeros(T, float)
+    mid = int(T/2)
+    x[mid] = 1.0
+    app, ders = ddwt(x, num_scales=5)
+
+    if spectral:
+        multilinespec(ders[1:])
     else:
-        data, fields = wfdb.rdsamp(filename)
-        numch = data.shape[1]
-        hdr = {
-            "fs": fields["fs"],
-            "adc_gain": np.array([1.0]*numch),
-            "baseline": np.array([0.0]*numch),
-            "samples": data.shape[0],
-            "channels": data.shape[1]
-        }
-
-        return data, hdr
+        multiplot(ders)
+        for i, x in enumerate(ders[1:]):
+            dt_theor = (2.0**i - 1)/2
+            dt_exrep = zcfind(x) - mid
+            print(dt_theor, dt_exrep)
 
 
-def build_args():
-    parser = ArgumentParser()
+def show_decomposition():
 
-    parser.add_argument(
-        '-t', '--time-range',
-        type=int,
-        default=6,
-        help='Time in seconds to display'
-    )
-
-    options, filenames = parser.parse_known_args()
-    if not filenames:
-        filenames.append("/Users/arseniy/SERDECH/data/ROXMINE/Rh2002")
-
-    if not filenames:
-        print("At least one input file should be specified")
-        sys.exit(1)
-
-    return options, filenames
+    sig, hdr = ecgread("/Users/arseniy/SERDECH/data/ROXMINE/Rh2024")
+    app, ders = ddwt(sig[:20000,0], num_scales=5)
+    multiplot(ders)
 
 
-def show_spectrums(recordname):
+def show_waves():
+    # Rh2022 = qr, noise
+    # Rh2021 - Rs, extracyc
+    # Rh2024 - p(q)RsT
+    # Rh2025 = rs
+    sig, header = ecgread("/Users/arseniy/SERDECH/data/ROXMINE/Rh1011")
+    fs = header["fs"]
+    if fs != 250:
+        print("Warning! fs={}".format(fs))
 
-    sig, fields = ecgread(recordname)
-    sp = mean_spectrum(sig[:, 0])
-    npoints = len(sp)
-    xf = np.linspace(0.0, 0.5 * fields["fs"], npoints)
+    s = sig[:20000,0]
 
-    fft_apert = 512
-    mains_att = 0.05
-    x, y = build_comb_filter(fields["fs"], fft_apert, mains_att)
+    metadata, debugdata = qrs_detection(
+        sig[:,:],
+        fs=header["fs"],
+        bias=header["baseline"],
+        gain=header["adc_gain"],
+        minqrs_ms=20)
 
-    umsig = mains_filter(
-        sig[:, 0],
-        fs=fields["fs"],
-        bias=fields["baseline"],
-        mains=50.0,
-        attenuation=mains_att,
-        aperture=fft_apert
-    )
+    newmeta = find_points(s, header["fs"], metadata)
 
-    sp2 = mean_spectrum(umsig)
+    plt.plot(s, "b")
 
-    plt.rcParams["figure.facecolor"] = "white"
+    qrsTypes = {}
 
-    fig, axarr = plt.subplots(2, 2)
-    axarr[0,0].plot(xf, sp)
-    axarr[1,0].plot(xf, sp2)
+    for qrs in newmeta:
 
-    happ = int(1 + fft_apert/2)
-    axarr[0,1].plot(x[:happ], y[:happ], 'r')
+        qrstype = qrs["qrsType"]
+        qrsTypes[qrstype] = qrsTypes.get(qrstype, 0) + 1
+
+        for k,v in qrs.get("waves", {}).items():
+            c = v["center"]
+            if c is not None:
+                plt.scatter(c, s[c])
+
+            if k in ("t", "p"):
+                lb = v["start"]
+                rb = v["end"]
+                if lb is not None and rb is not None:
+                    plt.plot(np.arange(lb, rb), s[lb:rb], "r")
+
+
+    plt.xlim((200,700))
+
+    print(qrsTypes)
 
     plt.show()
-
-
-def show_bmains(recordname, tend):
-
-    # загрузили сигнал
-    sig, hdr = ecgread(recordname)
-    fs = hdr["fs"]
-
-    unbias = fix_baseline(
-        sig,
-        hdr["fs"],
-        bias_window_ms=1500
-    )
-
-    corr = mains_filter(
-        unbias,
-        hdr["fs"],
-        hdr["baseline"],
-        mains=50.0,
-        attenuation=0.01,
-        aperture=512)
-
-    # номера начального и конечного отсчета для отображения
-    N1 = 0
-    N2 = int(tend*fs)
-
-    tt = np.linspace(float(N1)/fs, float(N2-1)/fs, N2-N1)
-
-    plt.style.use("ggplot")
-    plt.rcParams["figure.facecolor"] = "white"
-    fig, axarr = plt.subplots(hdr["channels"], 1, sharex=True)
-
-    for chan, signal in signal_channels(sig):
-
-        signal = (signal - hdr["baseline"][chan]) / hdr["adc_gain"][chan]
-        corsig = (corr[N1:N2, chan] - hdr["baseline"][chan]) / hdr["adc_gain"][chan]
-
-        axarr[chan].plot(tt, signal[N1:N2], "b")
-        axarr[chan].plot(tt, corsig, "r:")
-
-
-    plt.show()
-
-
-def show_qrs(recordname, tend):
-
-    # загрузили сигнал
-    sig, hdr = ecgread(recordname)
-    fs = hdr["fs"]
-
-    sig = fix_baseline(
-        sig,
-        fs=fs,
-        bias_window_ms=1500
-    )
-
-    # следующие две строчки для наглядности. В реальной прорамме достаточно
-    # вызвать
-    # meta, foo = qrs_detection(sig, fs)
-
-    ptstat = qrs_preprocessing(sig, fs)
-    meta, strobe = qrs_detection(
-        sig,
-        fs=fs,
-        bias=hdr["baseline"],
-        gain=hdr["adc_gain"])
-
-    # номера начального и конечного отсчета для отображения
-    N1 = 0
-    N2 = int(tend*fs)
-
-    tt = np.linspace(float(N1)/fs, float(N2-1)/fs, N2-N1)
-
-    plt.style.use("ggplot")
-    plt.rcParams["figure.facecolor"] = "white"
-    fig, axarr = plt.subplots(hdr["channels"]+1, 1, sharex=True)
-
-    for chan, signal in signal_channels(sig):
-
-        signal = (signal - hdr["baseline"][chan]) / hdr["adc_gain"][chan]
-
-        axarr[chan].plot(tt, signal[N1:N2])
-
-        # пометим R-зубцы
-        rx = []
-        ry = []
-        for x in meta:
-            r_pos = x["r_wave_center"][chan]
-            if tt[0] <= r_pos <= tt[-1]:
-                rx.append(r_pos)
-                ry.append(signal[int(rx[-1]*fs)])
-
-        axarr[chan].scatter(rx, ry, c="r")
-        axarr[chan].plot(tt,strobe[N1:N2])
-
-    # решающая статистика и стробы QRS-комплексов
-    axarr[hdr["channels"]].plot(tt,ptstat[N1:N2])
-    # axarr[hdr["channels"]].plot(tt,strobe[N1:N2])
-
-    plt.show()
-
-
-def main():
-    options, filenames = build_args()
-
-    #show_spectrums(sys.argv[1])
-    show_qrs(filenames[0], options.time_range)
-    #show_bmains(filenames[0], options.time_range)
-
 
 if __name__ == "__main__":
-    main()
+
+    #show_filter_responses()
+    #show_decomposition()
+    show_waves()
+
