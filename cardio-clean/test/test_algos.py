6e3e800e0403f75793f805867ad471c5dc24c850
afanasenko <afanasenko@smedx.com>
2018-07-01 15:21:22 +0300

    Добавлен новый тест

---
+++ cardio-clean/test/test_algos.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+from cardio_clean.metadata import *
+
+
+def test_conv():
+    fs = 250
+    smp = fs
+
+    assert ms_to_samples(samples_to_ms(smp, fs), fs) == smp
+    assert samples_to_ms(smp, fs) == 1.0
