71c2b6942b5175605f92559551d18d58dd391bd6
afanasenko <afanasenko@smedx.com>
2018-08-21 23:54:51 +0300

    Обновлены примеры

--- cardio-clean/test/demo_missing_waves.py
+++ cardio-clean/test/demo_missing_waves.py
@@ -7,47 +7,60 @@ from matplotlib import pyplot as plt
 from cardio_clean.wavdetect import find_points
 from cardio_clean.metadata import *
 
+from cardio_clean.sigbind import fix_baseline
 from cardio_clean.qrsdetect import qrs_detection
 from cardio_clean.util import ecgread
 
 
-def plot_with_markup(s, fs, metadata, chan):
+def plot_with_markup(sig, fs, metadata):
 
-    plt.cla()
+    numch = len(metadata[0]["r_pos"])
 
-    pt_keys = {"q_pos": "r","r_pos": "b", "s_pos": "b", "p_pos": "y", "t_pos":
+    pt_keys = {"q_pos": "b","r_pos": "r", "s_pos": "b", "p_pos": "y", "t_pos":
     "g"}
 
-    left_p = max(0, metadata[0]["qrs_center"]*fs - 100)
-    right_p = min(s.shape[0]-1, metadata[-1]["qrs_center"]*fs + 100)
-    plt.plot(s[int(left_p):int(right_p),chan], "b")
+    left_p = max(0, metadata[0]["qrs_center"]*fs - 120)
+    right_p = min(sig.shape[0]-1, metadata[-1]["qrs_center"]*fs + 120)
 
-    for ncycle, qrs in enumerate(metadata):
+    tscale = 1.0 / fs
 
-        for k in pt_keys:
-            point = qrs[k][chan]
-            if point is not None:
-                plt.scatter(point, s[point, chan], c=pt_keys[k])
+    f, ax = plt.subplots(numch, 1, sharex="col")
 
-        # подсветка P-зубца
-        lb = qrs["p_start"][chan]
-        rb = qrs["p_end"][chan]
-        if all((lb, rb)):
-            plt.plot(np.arange(lb, rb), s[lb:rb, chan], "y")
+    for chan, s in signal_channels(sig):
+        lb = int(left_p)
+        rb = int(right_p)
+        ax[chan].plot(np.arange(lb, rb)*tscale, s[lb:rb], "b")
 
-        # подсветка T-зубца
-        lb = qrs["t_start"][chan]
-        rb = qrs["t_end"][chan]
-        if all((lb, rb)):
-            plt.plot(np.arange(lb, rb), s[lb:rb, chan], "g")
+        for ncycle, qrs in enumerate(metadata):
 
-        lb = qrs["st_start"][chan]
-        rb = qrs["st_end"][chan]
-        if all((lb, rb)):
-            plt.plot(np.arange(lb, rb), s[lb:rb, chan], "r")
+            for k in pt_keys:
+                point = qrs[k][chan]
+                if point is not None:
+                    ax[chan].scatter(point*tscale, s[point], c=pt_keys[k])
 
-    plt.xlim((left_p, right_p))
-    plt.grid(True)
+            # подсветка R-зубца
+            lb = qrs["r_start"][chan]
+            rb = qrs["r_end"][chan]
+            if all((lb, rb)):
+                ax[chan].plot(np.arange(lb, rb) * tscale, s[lb:rb], "r")
+
+            # подсветка P-зубца
+            lb = qrs["p_start"][chan]
+            rb = qrs["p_end"][chan]
+            if all((lb, rb)):
+                ax[chan].plot(np.arange(lb, rb)*tscale, s[lb:rb], "y")
+
+            # подсветка T-зубца
+            lb = qrs["t_start"][chan]
+            rb = qrs["t_end"][chan]
+            if all((lb, rb)):
+                ax[chan].plot(np.arange(lb, rb)*tscale, s[lb:rb], "g")
+
+            lb = qrs["st_start"][chan]
+            rb = qrs["st_end"][chan]
+            if all((lb, rb)):
+                ax[chan].plot(np.arange(lb, rb)*tscale, s[lb:rb], "r")
+
     plt.show()
 
 
@@ -58,7 +71,11 @@ def show_waves(filename):
     if fs != 250:
         print("Warning! fs != 250 Hz, fs={}".format(fs))
 
-    s = sig[:,0]
+    sig = fix_baseline(
+        sig,
+        fs=fs,
+        bias_window_ms=1500
+    )
 
     metadata = qrs_detection(
         sig[:,:],
@@ -74,49 +91,31 @@ def show_waves(filename):
 
     metadata_postprocessing(
         metadata,
-        s,
+        sig,
         header
     )
-
-    qrsTypes = {}
-    stcount=0
 
     suspicious = []
 
     for ncycle, qrs in enumerate(metadata):
 
-        qrstype = qrs["qrsType"]
-        qrsTypes[qrstype] = qrsTypes.get(qrstype, 0) + 1
-
-        lb = qrs["st_start"][0]
-        rb = qrs["st_end"][0]
-        if all((lb, rb)):
-            stcount += 1
-        else:
+        if max(qrs["qt_duration"]) > 1.0:
             suspicious.append(ncycle)
 
     missing_hrt = [i for i,x in enumerate(metadata) if x["heartrate"] is None]
     print("Heartrate missing in beats\n{}".format(missing_hrt))
 
-    print(qrsTypes)
-
-    stdur = [qrs["st_duration"][0] for qrs in metadata if
-             qrs["st_duration"][0] and qrs["st_duration"][0] is not None]
-
-    print("{} cycles, {} ST segments, avg. {} ms of {} items".format(
-        len(metadata),
-        stcount,
-        np.mean(stdur),
-        len(stdur)
-    ))
-
     for sus in suspicious:
 
-        print("display cycle {}".format(sus))
-        i1 = max(0, sus-1)
+        print("display cycle {}, QT = {}".format(
+            sus,
+            metadata[sus]["qt_duration"]
+        ))
+        i1 = sus
         i2 = min(len(metadata), sus+2)
 
-        plot_with_markup(sig, fs, metadata[i1:i2], 0)
+        plot_with_markup(sig, fs, metadata[i1:i2])
+        # plot_with_markup(sig, fs, [metadata[sus]])
 
         print("Show next?")
         if sys.stdin.read(1).strip() == "n":
@@ -128,8 +127,8 @@ if __name__ == "__main__":
     # Rh2021 - Rs, extracyc
     # Rh2024 - p(q)RsT
     # Rh2025 = rs
-    # filename = "/Users/arseniy/SERDECH/data/ROXMINE/Rh2024"
-    filename = "TestFromDcm.ecg"
+    filename = "/Users/arseniy/SERDECH/data/ROXMINE/Rh2024"
+    # filename = "TestFromDcm.ecg"
 
     if len(sys.argv) > 1:
         filename = sys.argv[0]
