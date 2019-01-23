3213f06fbdc8e3c64112a8a34e5bb9ce0cee94df
afanasenko <afanasenko@smedx.com>
2019-01-23 18:56:31 +0300

    Унификация расчетов, добавление SDNNi

--- cardio-clean/cardio_clean/statvariation.py
+++ cardio-clean/cardio_clean/statvariation.py
@@ -1,99 +1,115 @@
 # coding: utf-8
 
-from scipy.fftpack import fft
 import numpy as np
-from config import config
-from metadata import is_artifact, is_pvc, safe_r_pos
-
-from matplotlib import pyplot as plt
 
 
-def rhythm_stats(metadata, fs):
+class RecusiveStat():
+    """utility class for recursive statistics calculation
+    """
+
+    def __init__(self):
+        self.count = 0
+        self.sum_val = 0.0
+        self.sum_sq = 0.0
+
+    def reset(self):
+        self.count = 0
+        self.sum_val = 0.0
+        self.sum_sq = 0.0
+
+    def size(self):
+        return self.count
+
+    def add(self, value):
+        self.sum_val += value
+        self.sum_sq += value*value
+
+    def average(self):
+        if self.count > 0:
+            return self.sum_val / self.count
+        else:
+            return None
+
+    def dispersion(self):
+        count = self.count
+        if count > 2:
+            disper = self.sum_sq / count - self.sum_val*self.sum_val / (count*count)
+            if disper >= 0:
+                return disper
+            else:
+                return 0.0
+        else:
+            return None
+
+    def std_dev(self):
+        disp = self.dispersion()
+        if disp is None:
+            return None
+        else:
+            return np.sqrt(disp)
+
+
+def rhythm_stats(metadata):
     """Расчет статистической вариабельности ритма
 
     :param metadata: список метаданных (только чтение). Требуемые ключи:
         - complex_type
         - RR
-    :param fs: частота дискретизации сигнала
 
-    :return: metrics: словарь, содержащий параметры вариабельности
-    в виде имя-значение. Вычисляемые параметры: RRmean, SDNN, pNN50, SDANN,
-    RMSSD, SDNNi. При отсутствии достаточного числа нормальных
+    :return: metrics: словарь (с фиксированным набором ключей), содержащий
+    параметры вариабельностив виде имя-значение.
+    Вычисляемые параметры: RRmean, SDNN, pNN50, SDANN, RMSSD, SDNNi.
+    При отсутствии достаточного числа нормальных
     qrs-комплексов все или часть параметров могут иметь значение None.
-    Параметр SDANN содержит список, остальные - скалярные значения.
     """
 
-    rrs = 0.0
-    rrs2 = 0.0
-    rrc = 0
+    nn_stat = RecusiveStat()
+    dnn_stat = RecusiveStat()
+    ann_stat = RecusiveStat()
+    five_stat = RecusiveStat()
+    nni_stat = RecusiveStat()
+
     count50 = 0
-
-    drrs = 0.0
-    drrs2 = 0.0
-    drrc = 0
-
-    delta = 0.05*fs
+    delta = 50      # порог на различие двух смежных интервалов
     last_nn = 0
 
     timestamp = metadata[0]["qrs_start"]
 
-    anns = 0.0
-    anns2 = 0.0
-    annc = 0
-
     for i, x in enumerate(metadata[:-1]):
 
         if x["qrs_start"] - timestamp > 300:
-
-            if rrc:
-                y = rrs/rrc
-                annc += 1
-                anns += y
-                anns2 += y * y
+            # начало нового 5-минутного интервала
+            if five_stat.size():
+                ann_stat.add(five_stat.average())
+                nni_stat.add(five_stat.std_dev())
+                five_stat.reset()
 
             timestamp = x["qrs_start"]
 
-
         if x["complex_type"] == "N" and metadata[i+1]["complex_type"] == "N":
-            rr = x["RR"]
+            # сразу переводим в мс
+            rr = x["RR"]*1000
 
             if rr is not None:
 
-                rrc += 1
-                rrs += rr
-                rrs2 += rr*rr
+                nn_stat.add(rr)
+                five_stat.add(rr)
 
                 if last_nn:
                     drr = rr - last_nn
                     if abs(drr) > delta:
                         count50 += 1
 
-                    drrc += 1
-                    drrs += drr
-                    drrs2 += drr*drr
+                    dnn_stat.add(drr)
 
                 last_nn = rr
 
-    if rrc:
-        rrm = rrs / rrc
-        rrsd = rrs2 / rrc - rrm * rrm
-        pnn = 100.0 * count50 / rrc
-    else:
-        rrm = None
-        rrsd = None
-        pnn = None
-
-    if drrc:
-        rrm = drrs / drrc
-        rmssd = drrs2 / drrc - rrm * rrm
-    else:
-        rmssd = None
-
-    if annc:
-        rrm = anns / annc
-        sdann = anns2 / annc - rrm * rrm
-    else:
-        sdann = None
+    rrm = nn_stat.average()
+    rrsd = nn_stat.std_dev()
+    pnn = 100.0 * count50 / nn_stat.size()
+    rmssd = dnn_stat.std_dev()
+    sdann = ann_stat.std_dev()
+    sdnni = nni_stat.average()
 
     return {
         "RRmean": rrm,
@@ -101,5 +117,5 @@ def rhythm_stats(metadata, fs):
         "pNN50": pnn,
         "SDANN": sdann,
         "RMSSD": rmssd,
-        "SDNNi": None
+        "SDNNi": sdnni
     }
